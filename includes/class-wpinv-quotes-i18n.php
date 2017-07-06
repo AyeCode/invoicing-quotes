e989fb62dcaf66f592cf3daca597fee21faaaeaa
lenovo <lenovo@192.168.5.36>
2017-07-06 16:40:22 +0500

    Plugin Launch

---
+++ includes/class-wpinv-quotes-i18n.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       https://wpgeodirectory.com
+ * @since      1.0.0
+ *
+ * @package    Wpinv_Quotes
+ * @subpackage Wpinv_Quotes/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Wpinv_Quotes
+ * @subpackage Wpinv_Quotes/includes
+ * @author     GeoDirectory Team <info@wpgeodirectory.com>
+ */
+class Wpinv_Quotes_i18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'wpinv-quotes',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
