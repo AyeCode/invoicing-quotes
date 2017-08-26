94d9e80737f08c74c1b59830d8d036062851f1a5
stiofan <info@wpgeodirectory.com>
2017-08-26 16:34:36 +0100

    Added tgm-plugin-activation class

--- wpinv-quote.php
+++ wpinv-quote.php
@@ -14,8 +14,8 @@
  *
  * @wordpress-plugin
  * Plugin Name:       Invoicing - Quotes
- * Plugin URI:        https://wpgeodirectory.com
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Plugin URI:        https://wpgeodirectory.com/
+ * Description:       Create quotes for customers, if accepted it will convert to an invoice that can be paid.
  * Version:           1.0.0
  * Author:            GeoDirectory Team
  * Author URI:        https://wpgeodirectory.com
@@ -84,6 +84,54 @@ function wpinv_invoice_plugin_notice()
 function run_wpinv_quotes()
 {
     if ( is_admin() && current_user_can( 'activate_plugins' ) &&  !class_exists( 'WPInv_Plugin' ) ) {
+        /**
+         * The plugin activation class that will install and activate Invoicing if missing.
+         */
+        require plugin_dir_path(__FILE__) . 'includes/class-tgm-plugin-activation.php';
+        add_action( 'tgmpa_register', 'wpinv_quotes_register_required_plugins' );
+
+        /**
+         * Require the Invoicing plugin to be installed.
+         */
+        function wpinv_quotes_register_required_plugins() {
+
+            /*
+             * Array of plugin arrays. Required keys are name and slug.
+             * If the source is NOT from the .org repo, then source is also required.
+             */
+            $plugins = array(
+                array(
+                    'name'      => 'Invoicing',
+                    'slug'      => 'invoicing',
+                    'required'  => true,
+                    'force_activation'   => true
+                )
+            );
+
+            /*
+             * Array of configuration settings. Amend each line as needed.
+             *
+             * TGMPA will start providing localized text strings soon. If you already have translations of our standard
+             * strings available, please help us make TGMPA even better by giving us access to these translations or by
+             * sending in a pull-request with .po file(s) with the translations.
+             *
+             * Only uncomment the strings in the config array if you want to customize the strings.
+             */
+            $config = array(
+                'id'           => 'wpinv-quotes',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+                'parent_slug'  => 'plugins.php',            // Parent menu slug.
+                'capability'   => 'manage_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+                'has_notices'  => true,                    // Show admin notices or not.
+                'dismissable'  => false,                    // If false, a user cannot dismiss the nag message.
+                'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+                'is_automatic' => true,                   // Automatically activate plugins after installation or not.
+                'message'      => '',                      // Message to output right before the plugins table.
+
+            );
+
+            tgmpa( $plugins, $config );
+        }
+
         add_action( 'admin_notices', 'wpinv_invoice_plugin_notice' ) ;
         return;
     }
